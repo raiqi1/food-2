a40fca05ce2339b10a26ea5bc58aea8e8cef9cea
KrOxYs <raifandibaiqi@gmail.com>
2024-01-06 17:19:12 +0700

    tambahan

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -81,7 +81,7 @@ export default function Header() {
       <div className="hidden md:flex items-center justify-between">
         <nav className="flex items-center gap-8 text-gray-500 font-semibold">
           <Link className="text-primary font-semibold text-2xl" href={'/'}>
-            ST PIZZA
+            PIZZA WEBSITE
           </Link>
           <Link href={'/'}>Home</Link>
           <Link href={'/menu'}>Menu</Link>
